4bf0cfbd5e1ac8e53b60abf8415059e77b760c69
Cryptic Stargazer <78745089+coinbrute@users.noreply.github.com>
2021-08-19 09:42:27 -0600

    Update zombieHelper.sol
    updated file with new fee and withdraw values and new ownerOf modifier

--- zombieHelper.sol
+++ zombieHelper.sol
@@ -4,21 +4,39 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
+  // fee to level up zombie
+  uint levelUpFee = 0.001 ether;
+
   // modifier to ensure zombie is at correct level to execute function called
   modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
     _;
   }
+  
+  // allows for withdrawal of funds from contract
+  function withdraw() external onlyOwner {
+    address _owner = owner();
+    _owner.transfer(address(this).balance);
+  }
+
+  // sets fee to pay to level up
+  function setLevelUpFee(uint _fee) external onlyOwner {
+    levelUpFee = _fee;
+  }
+
+  // levels up zombie
+  function levelUp(uint _zombieId) external payable {
+    require(msg.value == levelUpFee);
+    zombies[_zombieId].level++;
+  }
 
   // changes name of zombie
-  function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId]);
+  function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) ownerOf(_zombieId) {
     zombies[_zombieId].name = _newName;
   }
 
   // changes dna of zombie
-  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId]);
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) ownerOf(_zombieId) {
     zombies[_zombieId].dna = _newDna;
   }
 
